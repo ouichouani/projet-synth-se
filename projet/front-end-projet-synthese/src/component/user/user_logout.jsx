7d4a8738b17fca5dec0598cd9c655e9cc1e19ab5
ouichouani <ouichouaniabdlhakim@gmal.com>
2025-06-04 21:53:32 +0100

    add user_logout style and logique to user_show , and update user_show style

--- projet/front-end-projet-synthese/src/component/user/user_logout.jsx
+++ projet/front-end-projet-synthese/src/component/user/user_logout.jsx
@@ -2,20 +2,22 @@ import axios from "axios";
 import { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { UserContext } from "../../provider/userContext";
+import styles from "../../css/user/user_logout.module.css";
+
 
 export default function UserLogout() {
-        const {setStoredUserfunction} = useContext(UserContext) ;
+    const { setStoredUserfunction } = useContext(UserContext);
 
 
     const [message, setMessage] = useState();
-    const redirectTo = useNavigate() ;
-    
-    const logout = async () => {
+    const redirectTo = useNavigate();
+
+    const handleClick = async () => {
         const token = localStorage.getItem('token');
-        
+
         try {
             const response = await axios.post('http://127.0.0.1:8000/api/user/logout',
-                null ,
+                null,
                 {
                     headers: {
                         'Authorization': `Bearer ${token}`,
@@ -23,20 +25,36 @@ export default function UserLogout() {
                 }
             );
             setMessage(response.data.message);
-            localStorage.removeItem('token') ;
-            setStoredUserfunction(null) ;
+            localStorage.removeItem('token');
+            setStoredUserfunction(null);
             redirectTo('/user/login');
 
         } catch (error) {
             setMessage(error.response.data.message);
         }
-    }
+    }        
+        const cancelLogout = () => {
+            const user_logout_container = document.getElementById("user_logout_container");
+            const user_logout_over_layer = document.getElementById("user_logout_over_layer");
+            user_logout_container.style.left = '-500px'
+            Object.assign(user_logout_over_layer.style, { backgroundColor: 'transparent', zIndex: "-9" })
+        }
 
-    return (
-        <div className="logout_container">
-        <p>are you shor you want to log out ?</p>
-        <button onClick={logout}>log out</button>
-        <p className="message_logout">{message}</p>
-        </div>
-    )
-}
+
+        return (
+            <>
+                <div className={styles.user_logout_container} id="user_logout_container">
+                    <p>are you show you want to log out ?</p>
+                    <div>
+                        <button onClick={handleClick} >yes</button>
+                        <button onClick={cancelLogout}>no</button>
+                    </div>
+                </div>
+
+                <div className={styles.user_logout_over_layer} onClick={cancelLogout} id="user_logout_over_layer">
+                </div>
+            </>
+        )
+
+
+    }
--- projet/front-end-projet-synthese/src/component/user/user_show.jsx
+++ projet/front-end-projet-synthese/src/component/user/user_show.jsx
@@ -4,6 +4,7 @@ import { Link, useParams } from "react-router-dom"
 import { UserContext } from "../../provider/userContext";
 import styles from '../../css/user/user_show.module.css'
 import axios from "axios";
+import UserLogout from "./user_logout";
 export default function UserShow() {
 
     const [user, setUser] = useState();
@@ -17,9 +18,9 @@ export default function UserShow() {
 
                 const response = await axios.get(`http://127.0.0.1:8000/api/user/${id}`)
                 setUser(response.data.data)
-            } ;
+            };
 
-            get_user() ;
+            get_user();
 
         } else {
             setUser(storedUser)
@@ -27,6 +28,13 @@ export default function UserShow() {
 
     }, [id]);
 
+    const handleLogout = () => {
+        const user_logout_container = document.getElementById("user_logout_container");
+        const user_logout_over_layer = document.getElementById("user_logout_over_layer");
+        Object.assign(user_logout_container.style, { left: "50%", transform: "translate(-50%)" })
+        Object.assign(user_logout_over_layer.style, { backgroundColor: 'rgba(0, 0, 0, 0.4)', zIndex: "9000" })
+
+    }
 
     return (
         <div className={styles.user_show_page}>
@@ -44,12 +52,15 @@ export default function UserShow() {
 
                 {id == storedUser?.id && (
                     <div className={styles.user_log_container}>
-                        <Link to={`/user/update/${user?.id}` } state={user} >update</Link>
-                        <Link to={'/user/logout'} >logout</Link>
+                        <Link to={`/user/update/${user?.id}`} state={user} >update</Link>
+                        {/* <Link to={'/user/logout'} >logout</Link> */}
+                        <button onClick={handleLogout}>logout</button>
                     </div>
                 )}
 
             </div>
+
+            <UserLogout />
 
         </div>
     )
