5f7ec4064f9ae2c77659921bb7eab534eccc2644
ouichouani <ouichouaniabdlhakim@gmal.com>
2025-06-11 22:06:03 +0100

    update comment_create logic , add a comment to a pin and update comment component style

--- projet/front-end-projet-synthese/src/component/comment/comment.jsx
+++ projet/front-end-projet-synthese/src/component/comment/comment.jsx
@@ -1,27 +1,24 @@
 
 import axios from "axios";
-import styles from "../../css/comment/comment.module.css" ;
+import styles from "../../css/comment/comment.module.css";
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import { ReactComponent as Arrow } from "../../svg/arrow.svg";
-import { useRef } from 'react';
+// import { ReactComponent as Arrow } from "../../svg/arrow.svg";
+// import { ReactComponent as AddComment } from "../../svg/add-comment.svg";
+// import { useRef } from 'react';
 
 export default function Comment() {
 
     const { id } = useParams();
     const [comments, setComments] = useState(null);
-    const [showComment, setShowComment] = useState(false);
+    const [commentContent, setCommentContent] = useState();
+
     const navigatTo = useNavigate();
-    const commentContainerRef = useRef(null);
-    const token = '';
+    const token = localStorage.getItem('token');
 
 
 
     useEffect(() => {
-
-        if (showComment === false) {
-            return;
-        }
 
         const fetch_comments = async () => {
             try {
@@ -29,7 +26,6 @@ export default function Comment() {
                     headers: {
                         'Content-Type': 'application/json',
                         'Accept': 'application/json',
-                        ...(token && { 'Authorization': `Bearer ${token}` }), // send token only if it exists
                     },
 
                 });
@@ -40,41 +36,61 @@ export default function Comment() {
         }
 
         fetch_comments();
-        console.log(comments)
 
-    }, [id, token, showComment]);
+    }, [id, token]);
+
+    const handleChange = (e) => {
+        setCommentContent(e.target.value);
+    };
+
+    const handleClick = () => {
+
+        if (!commentContent.trim()) return;
 
 
-    const handleClick = (e) => {
-        e.stopPropagation();
+        const formData = new FormData();
+        formData.append('content', commentContent.trim());
+        formData.append('pin_id', id);
 
-        const target = e.currentTarget;
-        setShowComment((prevShowComment) => {
-            const newShowComment = !prevShowComment;
 
-            Object.assign(target.style, {
-                transform: newShowComment ? 'rotate(180deg)' : 'rotate(0deg)'
-            });
 
-            // Animate comment container
-            setTimeout(() => {
-                if (commentContainerRef.current) {
-                    commentContainerRef.current.style.height = newShowComment ? '100%' : '0px';
-                }
-            }, 0);
-            return newShowComment;
-        });
+        const add_comments = async () => {
+            try {
+                const response = await axios.post(`http://127.0.0.1:8000/api/comment`, formData,
+                    {
+                        headers: {
+                            'Content-Type': 'application/json',
+                            'Accept': 'application/json',
+                            ...(token && { 'Authorization': `Bearer ${token}` }), // send token only if it exists
+                        },
+
+                    });
+                setComments((prevComment) => ([...prevComment, response.data.data]));
+            } catch (error) {
+
+                console.log('error : ', error.message)
+            };
+        }
+
+        add_comments();
+
+        setCommentContent('') ;
+
+
+
     };
 
     return (
+
         <div className={styles.comments}>
             <div className={styles.comments_title}>
 
                 <h2>comments</h2>
-                <Arrow onClick={handleClick} width={"20px"} height={'30px'} />
+                <p>{comments?.length}</p>
 
             </div>
-            <div className={styles.comment_container} ref={commentContainerRef}>
+
+            <div className={styles.comment_container} >
                 {comments?.map((comment) => (
                     <div key={comment.id}>
                         <div className={styles.comment_user_section} onClick={() => navigatTo(`/user/show/${comment.user.id}`)}>
@@ -82,10 +98,18 @@ export default function Comment() {
                             <span>{comment.user.name || 'feetching ...'}</span>
                         </div>
                         <p>{comment.content}</p>
-                        <hr />
                     </div>
                 ))}
             </div>
+
+            <label htmlFor="add_comment_input" id="add_comment_label" >
+                <input type="text" value={commentContent} className={styles.add_comment_input} id="add_comment_input" onChange={handleChange} placeholder="write your comment here ..." />
+                <p onClick={handleClick}>send</p>
+            </label>
+
         </div>
+
+
     )
 }
+
